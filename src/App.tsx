6ec5a164943b1cdfe8a4f0bf87246f85b7cde227
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-10-14 19:01:17 +0900

    feat: PPD calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,205 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useMemo, useState } from 'react';
+import type { ChangeEventHandler } from 'react';
 import './App.css';
+import {
+  Box,
+  VStack,
+  HStack,
+  Input,
+  Text,
+  Divider,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Td,
+  Th,
+} from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [heightInPixels, setHeightInPixels] = useState<number>();
+  const [widthInPixels, setWidthInPixels] = useState<number>();
+  const [screenHeight, setScreenHeight] = useState<number>();
+  const [screenWidth, setScreenWidth] = useState<number>();
+  const [distanceToScreen, setDistanceToScreen] = useState<number>();
+
+  const handleHeightChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '') {
+      setHeightInPixels(undefined);
+    } else {
+      setHeightInPixels(Number(e.target.value));
+    }
+  };
+
+  const handleWidthChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '') {
+      setWidthInPixels(undefined);
+    } else {
+      setWidthInPixels(Number(e.target.value));
+    }
+  };
+
+  const handleSceenWidthChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '') {
+      setScreenWidth(undefined);
+    } else {
+      setScreenWidth(Number(e.target.value));
+    }
+  };
+
+  const handleSceenHeightChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '') {
+      setScreenHeight(undefined);
+    } else {
+      setScreenHeight(Number(e.target.value));
+    }
+  };
+
+  const handleDistanceChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.value === '') {
+      setDistanceToScreen(undefined);
+    } else {
+      setDistanceToScreen(Number(e.target.value));
+    }
+  };
+
+  /**
+   * diagonal resolution based on heghtInPixels and widthInPixels
+   */
+
+  const calculatedDiagonalPiexels = useMemo(() => {
+    if (heightInPixels && widthInPixels) {
+      return Math.sqrt(heightInPixels ** 2 + widthInPixels ** 2);
+    }
+  }, [heightInPixels, widthInPixels]);
+
+  /**
+   * diagonal screen size based on screenHeight and screenWidth
+   */
+  const calculatedDiagonalScreenSize = useMemo(() => {
+    if (screenHeight && screenWidth) {
+      return Math.sqrt(screenHeight ** 2 + screenWidth ** 2);
+    }
+  }, [screenHeight, screenWidth]);
+
+  /**
+   * pixcel per centimeter based on diagonal resolution and diagonal screen size
+   */
+  const pixcelPerCentimeter = useMemo(() => {
+    if (calculatedDiagonalPiexels && calculatedDiagonalScreenSize) {
+      return calculatedDiagonalPiexels / calculatedDiagonalScreenSize;
+    }
+  }, [calculatedDiagonalPiexels, calculatedDiagonalScreenSize]);
+
+  /**
+   * centimter per degree based on distance to screen
+   */
+  const centimeterPerDegree = useMemo(() => {
+    if (distanceToScreen) {
+      return 2 * Math.tan(getTanFromDegrees(0.5)) * distanceToScreen;
+    }
+  }, [distanceToScreen]);
+
+  /**
+   * PPD (pixel per degree) based on diagonal resolution, diagonal screen size and distance to screen
+   */
+  const calculatedPPD = useMemo(() => {
+    if (pixcelPerCentimeter && centimeterPerDegree) {
+      return pixcelPerCentimeter * centimeterPerDegree;
+    }
+  }, [calculatedDiagonalPiexels, centimeterPerDegree]);
+
+  function getTanFromDegrees(degrees: number) {
+    return Math.tan((degrees * Math.PI) / 180);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <VStack spacing={4}>
+        <Text>解像度</Text>
+        <HStack>
+          <Box>
+            <Text>縦 (ピクセル数)</Text>
+            <Input
+              type="number"
+              value={heightInPixels}
+              onChange={handleHeightChange}
+            />
+          </Box>
+
+          <Box>
+            <Text>横 (ピクセル数)</Text>
+            <Input
+              type="number"
+              value={widthInPixels}
+              onChange={handleWidthChange}
+            />
+          </Box>
+        </HStack>
+
+        <Text>スクリーンサイズ</Text>
+        <HStack>
+          <Box>
+            <Text>縦 (cm)</Text>
+            <Input
+              type="number"
+              value={screenHeight}
+              onChange={handleSceenHeightChange}
+            />
+          </Box>
+
+          <Box>
+            <Text>横 (cm)</Text>
+            <Input
+              type="number"
+              value={screenWidth}
+              onChange={handleSceenWidthChange}
+            />
+          </Box>
+        </HStack>
+
+        <Box>
+          <Text>画面までの距離 (cm)</Text>
+          <Input
+            type="number"
+            value={distanceToScreen}
+            onChange={handleDistanceChange}
+          />
+        </Box>
+
+        <Divider />
+
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>項目</Th>
+              <Th>数値</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>対角線上のピクセル数</Td>
+              <Td>{calculatedDiagonalPiexels?.toFixed(2)}</Td>
+            </Tr>
+            <Tr>
+              <Td>対角線上のスクリーンサイズ</Td>
+              <Td>{calculatedDiagonalScreenSize?.toFixed(2)}</Td>
+            </Tr>
+            <Tr>
+              <Td>1cm当たりのピクセル数</Td>
+              <Td>{pixcelPerCentimeter?.toFixed(2)}</Td>
+            </Tr>
+            <Tr>
+              <Td>視野角1度あたりのcm</Td>
+              <Td>{centimeterPerDegree?.toFixed(2)}</Td>
+            </Tr>
+            <Tr>
+              <Td>PPD (pixel per degree)</Td>
+              <Td>{calculatedPPD?.toFixed(2)}</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </VStack>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const rootEl = document.getElementById('root');
 rootEl &&
   ReactDOM.createRoot(rootEl).render(
     <React.StrictMode>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </React.StrictMode>,
   );
